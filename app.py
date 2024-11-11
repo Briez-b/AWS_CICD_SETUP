e2e26819f4b0f8b4dc19296f9ddb3ca0bfbcbb2c
Yauheni Bryshten <zhenyabrishtenpl@gmail.com>
2024-11-11 19:40:05 +0100

    New

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! Succesfuly deployed with full CI/CD!'
+    return 'I am gay'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
